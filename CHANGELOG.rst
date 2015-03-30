c6c291bc12fcb9e41c612cfc68d141b094554f13
Todd Wolfson <todd@twolfson.com>
2015-03-30 11:40:14 -0500

    Release 1.0.1

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,3 +1,5 @@
 resolve-link changelog
 ======================
+1.0.1 - Fixed bad Travis CI URL
+
 1.0.0 - Initial release
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='resolve_link',
-    version='1.0.0',
+    version='1.0.1',
     description='Resolve complete/partial URLs against a canonical target URL',
     long_description=open('README.rst').read(),
     keywords=[
